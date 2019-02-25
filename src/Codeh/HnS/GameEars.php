38941a3ce33c464cbdbebbc359d451e48ab3ed7c
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-02-25 08:54:03 +0800

    Update GameEars.php
    To be edited

--- src/Codeh/HnS/GameEars.php
+++ src/Codeh/HnS/GameEars.php
@@ -11,6 +11,7 @@ use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 
 use pocketmine\event\block\BlockBreakEvent;
@@ -32,70 +33,100 @@ class GameEars implements Listener{
 		  $this->main = $core;
     }
     
-	public function onLine(PlayerJoinEvent $event)
+	public function onJoin(PlayerJoinEvent $event) : void
 	{
 		$player = $event->getPlayer();
-		$name = $player->getName();
-		if($this->main->data->isCustomUsername($name) == false)//checks if player is not yet registered
+		if(in_array($player->getLevel()->getFolderName(), $this->rbharenas))
 		{
-			$string = $this->main->config->getNested("otherMessages.useRegister");
-			$string = str_replace("{NL}", TF::EOL, $string);
-			$player->sendMessage($string);
-		} else {
+			$this->leaveArena($player);
+		}
+	}
+	
+	public function onQuit(PlayerQuitEvent $event) : void
+	{
+		$player = $event->getPlayer();
+		if(in_array($player->getLevel()->getFolderName(), $this->rbharenas))
+		{
+			$this->leaveArena($player);
+		}
+	}
 
-			if($this->main->remember && $this->main->data->getIpAddress($name) == $player->getAddress())
-			{
-				return $this->main->LoginSuccess($player);
-			}
+	public function onBlockBreak(BlockBreakEvent $event)
+	{
+		$player = $event->getPlayer();
+		$level = $player->getLevel()->getFolderName(); 
+		if(in_array($level, $this->rbharenas))
+		{
+			$event->setCancelled();
+		}
+	}
 
-			$string = $this->main->config->getNested("otherMessages.useLogin");
-			$string = str_replace("{NL}", TF::EOL, $string);
-			$player->sendMessage($string);
-			
-			if($this->main->grace > 0)
+	public function onBlockPlace(BlockPlaceEvent $event)
+	{
+		$player = $event->getPlayer();
+		$level = $player->getLevel()->getFolderName(); 
+		if(in_array($level, $this->rbharenas))
+		{
+			$event->setCancelled();
+		}
+	}
+	
+	public function onDamage(EntityDamageEvent $event)
+	{
+		if($event instanceof EntityDamageByEntityEvent)
+		{
+			if($event->getEntity() instanceof Player && $event->getDamager() instanceof Player)
 			{
-				if(!in_array($name, $this->main->notLoggedIn))
+				$a = $event->getEntity()->getName(); $b = $event->getDamager()->getName();
+				if(array_key_exists($a, $this->iswaitingrbh) || array_key_exists($b, $this->iswaitingrbh))
 				{
-					$this->main->notLoggedIn[$name] = $this->main->microtime_int();
+					$event->setCancelled();
+					return true;
 				}
+				if(in_array($a, $this->isplayingrbh) && in_array($event->getEntity()->getLevel()->getFolderName(), $this->rbharenas))
+				{
+					$event->setCancelled(false); //for other plugin's cancelling damage event
+
+					if($event->getCause() == 2)
+					{
+						$event->setDamage(0.0); //hack, to remove damage on projectile hit entity event
+					}
+
+					if($event->getDamage() >= $event->getEntity()->getHealth())
+					{
+						$event->setDamage(0.0); //hack, to avoid players from getting killed
+						$event->setCancelled();
+						$inv = $event->getDamager()->getInventory();
+						if(!$inv->contains( Item::get(Item::ARROW) ))
+						{
+							$inv->addItem( $this->getArrow() );
+						}
+						$this->addKill($event->getDamager()->getName()); $this->notifyPlayer($event->getDamager(), 1);
+						$this->addDeath($event->getEntity()->getName()); $this->notifyPlayer($event->getEntity(), 2);
+						$this->randSpawn($event->getEntity(), $event->getEntity()->getLevel()->getFolderName());
+					}
+				}	
+				return true;
+			}
+		} else {
+			$a = $event->getEntity()->getName();
+			if(in_array($a, $this->isplayingrbh) || array_key_exists($a, $this->iswaitingrbh))
+			{
+				return $event->setCancelled();
 			}
 		}
-
 	}
 	
-	public function offLine(PlayerQuitEvent $event) : void
-	{
-		$name = $event->getPlayer()->getName();
-		if(in_array($name, $this->main->notLoggedIn))
-		{
-			unset($this->main->notLoggedIn[$name]);
-		}
-	}
-	
-	public function ifMoving(PlayerMoveEvent $event) : void
-	{
-		if($event->getPlayer()->isLoggedIn == false) $event->setCancelled();
-	}
-	
-	public function ifChatting(PlayerChatEvent $event) : void
+	public function testingCommands(PlayerCommandPreprocessEvent $event)
 	{
 		if($event->getPlayer()->isLoggedIn == false)
 		{
-			$event->setCancelled();
-			$event->getPlayer()->sendMessage($this->main->config->getNested("otherMessages.notLoggedIn"));
+				$command = explode(" ", $event->getMessage());
+				if($command[0] == "/login" || $command[0] == "/register") return;
+				$event->setCancelled();
+				$event->getPlayer()->sendMessage($this->main->config->getNested("otherMessages.notLoggedIn"));
 		}
 	}
-	
-	public function ifTryingCommands(PlayerCommandPreprocessEvent $event)
-    {
-		if($event->getPlayer()->isLoggedIn == false)
-		{
-			$command = explode(" ", $event->getMessage());
-			if($command[0] == "/login" || $command[0] == "/register") return;
-			$event->setCancelled();
-			$event->getPlayer()->sendMessage($this->main->config->getNested("otherMessages.notLoggedIn"));
-		}
-    }
 }
 
 ?>
